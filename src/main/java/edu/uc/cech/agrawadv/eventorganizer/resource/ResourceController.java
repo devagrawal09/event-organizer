6e5512b6514db98c01b600f2a76f708cdcbb5be7
Colin Vitols <54523046+vitolscolin@users.noreply.github.com>
2021-11-16 18:50:54 -0500

    added resource persistence to db.

--- src/main/java/edu/uc/cech/agrawadv/eventorganizer/resource/ResourceController.java
+++ src/main/java/edu/uc/cech/agrawadv/eventorganizer/resource/ResourceController.java
@@ -1,4 +1,31 @@
 package edu.uc.cech.agrawadv.eventorganizer.resource;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/events")
 public class ResourceController {
+
+	public ResourceService resourceService;
+
+	public ResourceController(ResourceService theResourceService) {
+		resourceService = theResourceService;
+	}
+
+	@GetMapping("/all")
+	public String listFaculties(Model theModel) {
+
+		// get faculty from db
+		List<Resource> theResources = resourceService.findAll();
+
+		// add faculties to the spring model
+		theModel.addAttribute("resource", theResources);
+
+		return "index";
+	}
 }
--- src/main/java/edu/uc/cech/agrawadv/eventorganizer/user/User.java
+++ src/main/java/edu/uc/cech/agrawadv/eventorganizer/user/User.java
@@ -1,7 +1,5 @@
 package edu.uc.cech.agrawadv.eventorganizer.user;
 
-import java.util.UUID;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
